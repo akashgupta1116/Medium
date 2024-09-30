e0e9dbb98b15d98f117499d5f280d1844347ea54
akashgupta1116 <yoakash6@gmail.com>
2024-09-30 23:44:28 +0530

    type fixes

--- frontend/frontend-app/src/components/Button.tsx
+++ frontend/frontend-app/src/components/Button.tsx
@@ -1,5 +1,13 @@
+import React from 'react';
+interface ButtonProps {
+    onClick? : ()=> void;
+    disabled? : boolean;
+    type? : string;
+    children: React.ReactNode
+}
+
 const Button = (props) => {
-    return props.disabled ? 
+    return props?.disabled ? 
     <button {...props} className="w-full h-12 text-white bg-gray-300 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800">
         {props.children}
     </button>
