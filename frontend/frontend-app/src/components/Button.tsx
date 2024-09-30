88e16cd885a21b165319cdc8b52caec2c2701f92
akashgupta1116 <yoakash6@gmail.com>
2024-09-30 23:46:05 +0530

    fix

--- frontend/frontend-app/src/components/Button.tsx
+++ frontend/frontend-app/src/components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
     children: React.ReactNode
 }
 
-const Button = (props) => {
+const Button: React.FC<ButtonProps> = (props) => {
     return props?.disabled ? 
     <button {...props} className="w-full h-12 text-white bg-gray-300 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800">
         {props.children}
